2e93596e0900fbb2d73579537ca0e0d2f148b1ef
Nassime Abdiou <nassime.abdiou@icloud.com>
2025-07-01 11:05:28 +0200

    Refactored example main

--- example/src/main/java/fr/nassime/nimbus/example/SimpleExample.java
+++ example/src/main/java/fr/nassime/nimbus/example/SimpleExample.java
@@ -1,17 +1,15 @@
 package fr.nassime.nimbus.example;
 
 import fr.nassime.nimbus.NimbusApplication;
-import fr.nassime.nimbus.api.annotations.NimbusApp;
+import fr.nassime.nimbus.annotations.NimbusApp;
+
+import java.io.IOException;
 
 @NimbusApp
 public class SimpleExample {
-    /**
-     * This is a simple example of a Nimbus application.
-     * It demonstrates how to create a basic Nimbus application with no additional features.
-     * To run this example, use the command: `java -jar example.jar`
-     */
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         NimbusApplication.run(SimpleExample.class, args);
     }
-}
 
+}
